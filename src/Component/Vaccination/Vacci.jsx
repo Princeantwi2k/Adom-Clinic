93a6e5fc4bf443e269e9de428c481abe33cc0a42
prince antwi <antwi2k@gmail.com>
2021-07-08 16:52:24 +0800

    Added Vacci.jsx

--- src/Component/Vaccination/Vacci.jsx
+++ src/Component/Vaccination/Vacci.jsx
@@ -12,21 +12,23 @@ const Vacci = () => {
             Clinic:
           </p>
           <ul className="page1">
-            <li>Chicken Pox (Varilrix)</li>
-            <li>Diptheria, Tetanus & Pertussis (DTaP)</li>
-            <li>Hepatitis A</li>
-            <li>Hepatitis B</li>
-            <li>Human Papilloma Virus (HPV)</li>
-            <li>Meningitis (Mencevax)</li>
-            <li>MMR (Measles Mumps & Rubella)</li>
-            <li>Pneumococal Conjugate Vaccine (PCV13 and PPV23)</li>
-            <li>Polio (Oral Polio Vaccine)</li>
-            <li>PPD (for Tuberculin Skin Testing/Mantoux)</li>
-            <li>Rabies (ARV)</li>
-            <li>Rotavirus (Rotarix)</li>
-            <li>Tetanus Diptheria (Td)</li>
-            <li>Tetanus Toxoid</li>
-            <li>Typhoid</li>
+            <li className="list">Chicken Pox (Varilrix)</li>
+            <li className="list">Diptheria, Tetanus & Pertussis (DTaP)</li>
+            <li className="list">Hepatitis A</li>
+            <li className="list">Hepatitis B</li>
+            <li className="list">Human Papilloma Virus (HPV)</li>
+            <li className="list">Meningitis (Mencevax)</li>
+            <li className="list">MMR (Measles Mumps & Rubella)</li>
+            <li className="list">
+              Pneumococal Conjugate Vaccine (PCV13 and PPV23)
+            </li>
+            <li className="list">Polio (Oral Polio Vaccine)</li>
+            <li className="list">PPD (for Tuberculin Skin Testing/Mantoux)</li>
+            <li className="list">Rabies (ARV)</li>
+            <li className="list">Rotavirus (Rotarix)</li>
+            <li className="list">Tetanus Diptheria (Td)</li>
+            <li className="list">Tetanus Toxoid</li>
+            <li className="list">Typhoid</li>
             <li>
               Pentavalent Vaccine (Haemophilus influenzae type B, whooping
               cough, tetanus, hepatitis B and diphtheria)
