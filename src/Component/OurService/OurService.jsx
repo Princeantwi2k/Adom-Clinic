0eeac7a14596babf8cfddf0250f0a0c4317fb135
prince antwi <antwi2k@gmail.com>
2021-07-08 16:51:38 +0800

    Added OurService.jsx

--- src/Component/OurService/OurService.jsx
+++ src/Component/OurService/OurService.jsx
@@ -8,30 +8,32 @@ const OurService = () => {
   return (
     <div>
       <div className="row">
-        <div className=" col-xm-12 col-sm-12 col-md-12">
-          <h1 className="head-tags">OUR SERVICES</h1>
+        <div className=" col-xs-12 col-sm-12 col-md-12">
+          <h1 className="head-tags">
+            OUR <span style={{ color: `blueviolet` }}>SERVICES</span>{" "}
+          </h1>
           <div className="icon-line"></div>
           <p className="head-tags">
             Adom Clinic offers a wide range of services to suit your needs
           </p>
         </div>
-        <div className="col-xm-12 col-sm-12 col-md-1"></div>
-        <div className=" col-xm-12 col-sm-12 col-md-3 icon">
+        <div className="col-xs-12 col-sm-12 col-md-1"></div>
+        <div className=" col-xs-12 col-sm-12 col-md-3 icon">
           <img src={icon} alt="icon" className="icon-img" />
           <h4 className="icon-lebel"> Specialist Consultations</h4>
-          <button className="btn btn-danger"> Learn more</button>
+          <button className="btn btn-danger buttom"> Learn more</button>
         </div>
-        <div className="col-xm-12 col-sm-12 col-md-3 icon">
+        <div className="col-xs-12 col-sm-12 col-md-3 icon">
           <img src={icon1} alt="icon" className="icon-img" />
           <h4 className="icon-lebel">Immigration Medicals</h4>
-          <button className="btn btn-danger"> Learn more</button>
+          <button className="btn btn-danger buttom"> Learn more</button>
         </div>
-        <div className="  col-xm-12 col-sm-12 col-md-3 icon">
+        <div className="  col-xs-12 col-sm-12 col-md-3 icon">
           <img src={icon2} alt="icon" className="icon-img" />
           <h4 className="icon-lebel"> AkaiLab - Laboratory</h4>
-          <button className="btn btn-danger"> Learn more</button>
+          <button className="btn btn-danger buttom"> Learn more</button>
         </div>
-        <div className="  col-xm-12 col-sm-12 col-md-1"></div>
+        <div className="  col-xs-12 col-sm-12 col-md-1"></div>
       </div>
     </div>
   );
