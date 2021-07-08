5495f3b8201948b583d5c5c025b0550718b5af8d
prince antwi <antwi2k@gmail.com>
2021-07-08 11:31:05 +0800

    added Home.jsx

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Caro from "../Carousel/Caro";
+import Values from "../OurValue/Vales";
+import About from "../Us/Us";
+import Service from "../OurService/OurService";
+
+const Home = () => {
+  return (
+    <div>
+      <Caro />
+      <Values />
+      <About />
+      <Service />
+    </div>
+  );
+};
+
+export default Home;
