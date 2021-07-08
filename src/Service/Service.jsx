9b9ec5a98e1406acdee380bb75574a1f784f1b0d
prince antwi <antwi2k@gmail.com>
2021-07-08 16:53:53 +0800

    Added Service.jsx

---
+++ src/Service/Service.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Vacci from "../Component/Vaccination/Vacci";
+
+const Service = () => {
+  return (
+    <div>
+      <Vacci />
+    </div>
+  );
+};
+
+export default Service;
