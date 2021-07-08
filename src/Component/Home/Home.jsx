61c19232bea453cadeaa39fe8895e31a539f53ee
prince antwi <antwi2k@gmail.com>
2021-07-08 16:50:16 +0800

    added Home.jsx

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -3,6 +3,7 @@ import Caro from "../Carousel/Caro";
 import Values from "../OurValue/Vales";
 import About from "../Us/Us";
 import Service from "../OurService/OurService";
+import Updates from "../Updates/Updates";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Values />
       <About />
       <Service />
+      <Updates />
     </div>
   );
 };
