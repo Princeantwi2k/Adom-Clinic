e6092e4b946222511fbc4963cb5d1ccf7e46968e
princeantwi2k <antwi2k@gmail.com>
2021-10-17 22:56:32 +0800

    added Us

--- src/Component/Us/Us.jsx
+++ src/Component/Us/Us.jsx
@@ -5,41 +5,18 @@ import "./Us.css";
 const Us = () => {
   return (
     <>
-      <div class="row us">
-        <div class="text-center"></div>
-        <div class="col-xs-6 col-sm-6 col-md-6">
-          <img src={pic5} class="img" />
+      <div className="container-fluid us">
+        <h2 className="topic"> Us</h2>
+        <div className="col-xs-6 col-sm-6 col-md-6">
+          <img src={pic5} alt="us" className="img" />
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-6">
-          <h2 className="topic"> Us</h2>
-          <p className="text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p className="text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-
-          <p className="text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+        <div className="col-xs-6 col-sm-6 col-md-6 ">
+          <div className="text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat,
+            ab? Animi cumque, pariatur, voluptas earum similique eveniet dolor
+            tempora odit iusto atque necessitatibus eos, illo perferendis iure
+            adipisci ut. Sapiente.
+          </div>
         </div>
       </div>
     </>
