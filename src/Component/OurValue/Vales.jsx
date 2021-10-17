7d1fa8c850c0540aea6ffcc21c7fe4936557af03
princeantwi2k <antwi2k@gmail.com>
2021-10-18 04:33:14 +0800

    added vales

--- src/Component/OurValue/Vales.jsx
+++ src/Component/OurValue/Vales.jsx
@@ -9,37 +9,39 @@ const Values = () => {
           Our <span style={{ color: `blueviolet` }}>Values</span>
         </h1>
         <div className="value-line"></div>
-        <div className="col-xs-12 col-sm-6 col-md-1"> </div>
-        <div className="col-xs-12 col-sm-6 col-md-3 service">
-          <i className="fa fa-medkit font" aria-hidden="true"></i>
-          <h4 className="head-topic">Preventive Healthcare</h4>
-          <div className="line"></div>
-          <p className="pargraph">
-            Adom Clinic values your health, so we encourage wellness at all
-            times, not just when you’re sick. We offer services to support your
-            healthy lifestyle.
-          </p>
+        <div className="container">
+          <div className="col-xs-6 col-sm-6 col-md-1"> </div>
+          <div className="col-xs-6 col-sm-6 col-md-3 service">
+            <i className="fa fa-medkit font" aria-hidden="true"></i>
+            <h4 className="head-topic">Preventive Healthcare</h4>
+            <div className="line"></div>
+            <p className="pargraph">
+              Adom Clinic values your health, so we encourage wellness at all
+              times, not just when you’re sick. We offer services to support
+              your healthy lifestyle.
+            </p>
+          </div>
+          <div className="col-xs-6 col-sm-6 col-md-3 service">
+            <i className="fa fa-id-card-o font" aria-hidden="true"></i>
+            <h4 className="head-topic">Community Health Education</h4>
+            <div className="line"></div>
+            <p className="pargraph">
+              Adom Clinic values our community. We support clinics in various
+              low-income communities. We also organize health awareness events
+              throughout the year.
+            </p>
+          </div>
+          <div className="col-xs-6 col-sm-6 col-md-3 service">
+            <i class=" font fa fa-female" aria-hidden="true"></i>
+            <h4 className="head-topic">Family Health</h4>
+            <div className="line"></div>
+            <p className="pargraph">
+              Adom Clinic values families, so we offer a range of services to
+              keep your family healthy and happy at any age.
+            </p>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-md-1"> </div>
         </div>
-        <div className="col-xs-12 col-sm-6 col-md-3 service">
-          <i className="fa fa-id-card-o font" aria-hidden="true"></i>
-          <h4 className="head-topic">Community Health Education</h4>
-          <div className="line"></div>
-          <p className="pargraph">
-            Adom Clinic values our community. We support clinics in various
-            low-income communities. We also organize health awareness events
-            throughout the year.
-          </p>
-        </div>
-        <div className="col-xs-12 col-sm-6 col-md-3 service">
-          <i class=" font fa fa-female" aria-hidden="true"></i>
-          <h4 className="head-topic">Family Health</h4>
-          <div className="line"></div>
-          <p className="pargraph">
-            Adom Clinic values families, so we offer a range of services to keep
-            your family healthy and happy at any age.
-          </p>
-        </div>
-        <div className="col-xs-12 col-sm-6 col-md-1"> </div>
       </div>
     </div>
   );
