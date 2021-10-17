6c9e99cbc9d6fd01fa44aead90e7d46c098e98e8
princeantwi2k <antwi2k@gmail.com>
2021-10-17 22:57:14 +0800

    added Contact

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -11,33 +11,33 @@ const Contact = () => {
 
       <div className="container">
         <div className="row">
-          <div className="col-md-12">
-            <div className="col-md-6">
+          <div className="col-xs-12 col-sm-12 col-md-12">
+            <div className="col-xs-6 col-sm-6 col-md-6">
               <h1 className="mind">Contact</h1>
               <h5 className="dede">Call, Whats'app or send us an E-mail</h5>
             </div>
-            <div className="col-md-6 blade">
-              <h2>
+            <div className="col-xs-6 col-sm-6 col-md-6 blade">
+              <h4>
                 <i className="fa fa-phone" aria-hidden="true">
                   {" "}
                   (+233)5503001103{" "}
                 </i>
-              </h2>
+              </h4>
               <h2>
                 <i className="fa fa-whatsapp" aria-hidden="true">
                   {" "}
                   0572211378
                 </i>
               </h2>
-              <h2>
+              <h4>
                 <i className="fa fa-envelope-o" aria-hidden="true">
                   {" "}
                   antwi2k@gmail.com
                 </i>
-              </h2>
+              </h4>
             </div>
           </div>
-          <div className="col-xs-6 col-sm-6 col-md-12">
+          <div className="col-xs-12 col-sm-12 col-md-12">
             <div className="col-xs-6 col-sm-6 col-md-6">
               <h1 className="mind">Social</h1>
               <h5 className="dede">
