5ba28a279993165ff21a0189353cbeb018b12d69
princeantwi2k <antwi2k@gmail.com>
2021-10-17 22:55:08 +0800

    added Lab

--- src/Component/Lab/Lab.jsx
+++ src/Component/Lab/Lab.jsx
@@ -19,38 +19,40 @@ const Lab = () => {
         both local and international companies. We use data-processing systems
         to ensure that reports are delivered to clients on time.
       </p>
-      <div className="row">
-        <div className="col-xs-6 col-sm-6 col-md-7">
-          <p className="lab-para">Our services include but not limited to:</p>
-          <ul>
-            <li className="lab-lists">
-              Haematology (routine and specialist labs)
-            </li>
-            <li className="lab-lists">
-              Chemical Pathology (routine and specialist lab
-            </li>
-            <li className="lab-lists">
-              Molecular Laboratory Techniques (PCR): Tuberculosis(IGRA), HPV,
-              HIV viral load, Gonorrhea and Chlamydia testing
-            </li>
-            <li className="lab-lists">
-              Immunology: Tumour Markers, Fertility screening and Hepatitis A
-              and B screening
-            </li>
-            <li className="lab-lists">Serological Testing</li>
-            <li className="lab-lists">Bacteriology Lab</li>
-            <li className="lab-lists">TB Lab</li>
-            <li className="lab-lists">
-              Mobile Phlebotomy: Experience our services in the privacy of your
-              home or office and at your convenience.
-            </li>
-            <li className="lab-lists">
-              Preventive Medicine (Executive Wellness screening)
-            </li>
-          </ul>
-        </div>
-        <div className="col-xs-6 col-sm-6 col-md-5">
-          <img src={labpic} alt="lab" className="lab-pic" />
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-6 col-sm-6 col-md-6">
+            <p className="lab-para">Our services include but not limited to:</p>
+            <ul>
+              <li className="lab-lists">
+                Haematology (routine and specialist labs)
+              </li>
+              <li className="lab-lists">
+                Chemical Pathology (routine and specialist lab
+              </li>
+              <li className="lab-lists">
+                Molecular Laboratory Techniques (PCR): Tuberculosis(IGRA), HPV,
+                HIV viral load, Gonorrhea and Chlamydia testing
+              </li>
+              <li className="lab-lists">
+                Immunology: Tumour Markers, Fertility screening and Hepatitis A
+                and B screening
+              </li>
+              <li className="lab-lists">Serological Testing</li>
+              <li className="lab-lists">Bacteriology Lab</li>
+              <li className="lab-lists">TB Lab</li>
+              <li className="lab-lists">
+                Mobile Phlebotomy: Experience our services in the privacy of
+                your home or office and at your convenience.
+              </li>
+              <li className="lab-lists">
+                Preventive Medicine (Executive Wellness screening)
+              </li>
+            </ul>
+          </div>
+          <div className="col-xs-6 col-sm-6 col-md-6">
+            <img src={labpic} alt="lab" className="lab-pic" />
+          </div>
         </div>
       </div>
     </div>
