9733443254acd66d696beef4700c823f364a0b8e
princeantwi2k <antwi2k@gmail.com>
2021-12-21 02:49:53 -0500

    added Us

--- src/Component/Us/Us.jsx
+++ src/Component/Us/Us.jsx
@@ -7,15 +7,17 @@ const Us = () => {
     <>
       <div className="container-fluid us">
         <h2 className="topic"> Us</h2>
-        <div className="col-xs-6 col-sm-6 col-md-6">
-          <img src={pic5} alt="us" className="img" />
-        </div>
-        <div className="col-xs-6 col-sm-6 col-md-6 ">
-          <div className="text">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat,
-            ab? Animi cumque, pariatur, voluptas earum similique eveniet dolor
-            tempora odit iusto atque necessitatibus eos, illo perferendis iure
-            adipisci ut. Sapiente.
+        <div className="row aboutUs">
+          <div className="col-xs-12 col-sm-12 col-md-6 ">
+            <img src={pic5} alt="us" className="Usimg" data-aos="flip-down" />
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-6 ">
+            <div className="text">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat,
+              ab? Animi cumque, pariatur, voluptas earum similique eveniet dolor
+              tempora odit iusto atque necessitatibus eos, illo perferendis iure
+              adipisci ut. Sapiente.
+            </div>
           </div>
         </div>
       </div>
